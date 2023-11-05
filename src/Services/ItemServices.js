f886e2acc3144922aabe5e54f08dbc9966909d56
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2023-11-06 01:33:47 +0200

    rf

--- src/Services/ItemServices.js
+++ src/Services/ItemServices.js
@@ -1,14 +1,18 @@
 
 import { Alert } from "react-native";
+
+// to be converted to functional component setOriginalNode.
 export class ItemServices {
     constructor(){}
-
+    
     // item methods
  deleteItem(params) {
     Alert.alert("Delete item", "comfirm to delete item", [
         {
           text: "Delete",
-          onPress: () => console.log("deleting"),
+          onPress: () => {
+            Alert.alert('Successful')
+        }
         },
          {
           text: "Cancel",
